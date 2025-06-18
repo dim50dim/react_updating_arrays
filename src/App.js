6f5460e01b234088da26129703838e31b6c1d033
¨dim50dim¨ <zheliezkov@gmail.com>
2025-06-18 08:17:50 +0200

    made component CounterList. there are two methods map there.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import List from './components/List';
 import ShapeEditor from './components/ShapeEditor';
+import CounterList from './components/CounterList';
 
 function App() {
   return (
    <>
-   <ShapeEditor />
+
+   <CounterList/>
    </>
   );
 }
